674068b3c17547bf2b23002b13181141a418c1f6
Jameal Samhouri <jameal.samhouri@noaa.gov>
2015-12-16 15:31:38 -0800

    Improvements to the github cheat sheet

--- Analysis/ses_network/github cheat sheet.R
+++ Analysis/ses_network/github cheat sheet.R
@@ -1,12 +1,12 @@
 # github cheat sheet
 # 
 # git pull origin master 
-# git push origin master 
+# git status = what have i changed
 # git add --all = add what i have done to be saved
 # git commit = save what i have done
-# git status = what have i changed
-# git log = what have i done
+# git push origin master 
 
+# git log = what have i done, view commit messages
 # if in doubt hit q
 
 # to start a commit message press i
