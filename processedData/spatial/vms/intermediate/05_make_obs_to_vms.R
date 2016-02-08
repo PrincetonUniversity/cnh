d893c4057969684a95d2e13d97921b3cdb8ab7dd
Emma Fuller <efuller@princeton.edu>
2016-02-08 13:21:20 -0800

    working on social shrimp behavioral segmentation

--- processedData/spatial/vms/intermediate/05_make_obs_to_vms.R
+++ processedData/spatial/vms/intermediate/05_make_obs_to_vms.R
@@ -1,47 +1,126 @@
 library(dplyr)
 library(tidyr)
+# make sure working directory is set to CNH/
 # load data ----
-  #! delete existing files
-  system("rm -f /Users/efuller/1/CNH/processedData/spatial/vms/intermediate/05_make_obs_to_vms/*.csv")
-  # load observer data
-  data_obs <- read.csv(
-    "/Users/efuller/Desktop/CNH/rawData/Observers/WCGOPobs/Samhouri_OBFTfinal_Allfisheries_ProcessedwFunction_2009-2012_110613.csv",stringsAsFactors = FALSE)
-  colnames(data_obs) <- tolower(colnames(data_obs))
-  data_obs <- data_obs[,-grep("spgrftob1",colnames(data_obs))[1]]
-
+# load condensed obs version, if doesn't exist load and format raw
+  if(file.exists("processedData/both/obs_data/obs.RDS")){
+    obs <- readRDS("processedData/both/obs_data/obs.RDS")
+  }else{
+    # load observer data
+    obs <- read.csv(
+      "rawData/Observers/WCGOPobs/Samhouri_OBFTfinal_Allfisheries_ProcessedwFunction_2009-2012_110613.csv",
+      stringsAsFactors = FALSE)
+    colnames(obs) <- tolower(colnames(obs))
+    obs <- obs[,-grep("spgrftob1",colnames(obs))[1]]
+    
+    obs$cg_num <- as.character(obs$cg_num)
+    saveRDS(obs, "processedData/both/obs_data/obs.RDS")
+  }
+  
+# load VMS data
   # choose time window
-  window = 24 # 0, 24, 36, 72, 168
+  window = 0 # 0, 24, 36, 72, 168
   
   # names of vessel tracks
-  path = paste0("/Users/efuller/Desktop/CNH/processedData/spatial/vms/intermediate/04_link_mets_vms/tw_",window,"hr/")
+  path = paste0("processedData/spatial/vms/intermediate/04_link_mets_vms/tw_",window,"hr/")
   names = dir(path)
-
+  
+  # drop any no_vms_landings names
   if(any(grepl("no_vms_landings", names))){
     names = names[-grep("no_vms_landings",names)]
     }
 
-# set up data objects ----
-# change observer cg.num to character to match classes
-data_obs$cg_num <- as.character(data_obs$cg_num)
-data_obs$d_date <- as.POSIXct(data_obs$d_date, format = "%")
+# double check time makes sense for set and up ----
+  # up and set times are decimal minutes. clean up
+  make_fullHrMin <- function(time_vector){
+    hour_vec <- sprintf("%02.0f",floor(time_vector))
+    min_vec <- sprintf("%02.0f",(time_vector - floor(time_vector))*60)
+    min_hour <- paste(hour_vec, min_vec, sep=":")
+    return(min_hour)
+  }
+  set_time <- make_fullHrMin(obs$set_time) 
+  up_time <- make_fullHrMin(obs$up_time)
+  
+  set_datetime <- as.POSIXct(
+    paste0(obs$set_year,"-",obs$set_month,"-",obs$set_day," ", set_time),
+    format= "%Y-%m-%d %H:%M", tz = "Etc/GMT-8"
+    )
+  
+  up_datetime <- as.POSIXct(
+    paste0(obs$up_year,"-",obs$up_month,"-",obs$up_day," ", up_time),
+    format= "%Y-%m-%d %H:%M", tz = "Etc/GMT-8"
+  )
+  
+  any(set_datetime>up_datetime) # should be FALSE
+  
+# double check departure and return times
+  # d/r_dates at midnight don't have any hours, add them
+  add_fullTime <- function(date_vec){
+    no_time <- which(nchar(date_vec)<11)
+    date_vec[no_time] <- paste(date_vec[no_time], "12:00:00 AM")
+    
+    full_time <- as.POSIXct(date_vec, format = "%m/%d/%Y %I:%M:%S %p", 
+                              tz = "Etc/GMT-8")
+    return(full_time)
+  }
+  d_dates <- add_fullTime(obs$d_date)
+  r_dates <- add_fullTime(obs$r_date)
+  
+  any(is.na(d_dates)) # should be false
+  any(is.na(r_dates)) # should be false
+  
+  all(d_dates < r_dates) # should be true
+  
+# double check set and return dates relative to depart and return
+  length(which(set_datetime<d_dates)) # bad
+  length(which(up_datetime<d_dates)) # good
+  
+  which(up_datetime>r_dates) # bad
+  which(set_datetime>r_dates) # bad, but all in the above
+  
+  # all is fixed gear  - could theoretically make sense 
+  # but how do observers know these times?
+  
+  table(obs$gear_type[which(up_datetime>r_dates)]) 
+  table(obs$sector[which(set_datetime<d_dates)]) 
+  
+  # save for further inspection
+  mismatch_time <- obs[c(which(up_datetime>r_dates), 
+                         which(set_datetime<d_dates)),]
+  write.csv(mismatch_time, "processedData/both/obs_data/mismatch_time.csv")
+  
+  # drop these for now
+  obs <- obs[-c(which(up_datetime>r_dates), 
+              which(set_datetime<d_dates)),]
 
-#! load vessel data
+  
+  
+# set up data objects ----
+# keep track of fish ticket, sector, vessel, port, and date for 
+# observed trips and amount of time spent fishing
+  
+  # keep track of trip data
+  total_trip_dat <- data.frame()
+#! load vessel data ----
+  
 for (i in 1:length(names)){
   ### Load up
   infile = names[i]
-  data_vms <- readRDS(paste0(path,infile))
+  vms <- readRDS(paste0(path,infile))
   
   # create vectors of known behavior
-  data_vms$fishing <- NA
-  data_vms$observed <- NA
+  vms$fishing <- NA
+  vms$observed <- NA
+  vms$sector <- NA # keep this for curiousity
+  vms$pcid <- NA # keep this for curiousity
 
   #! Ensure vms time to correct time zone
-  data_vms$date.time <- as.POSIXct(data_vms$date.time,tz="Etc/GMT-8")
+  vms$date.time <- as.POSIXct(vms$date.time,tz="Etc/GMT-8")
   
   #! find vms - obs matches
-  ID = which(data_obs$cg_num %in% unique(data_vms$doc.num))
+  ID_found = any(obs$cg_num %in% unique(vms$doc.num))
   
-  if (length(ID)>0){
+  if (ID_found){
 
     # subset observer dataframe (all recorded fishing events [start and stop times])
     # In v_obs look for set_time and up_time, this is the timing of a fishing event
@@ -49,84 +128,77 @@ for (i in 1:length(names)){
     # note: v_obs is in decimal time (change to mins secs)
     
     #! Streamline this by getting rid of duplicates
-    v_obs <- data_obs %>%
-      filter(cg_num == unique(data_vms$doc.num)) %>%
+    v_obs <- obs %>%
+      filter(cg_num == unique(vms$doc.num)) %>%
       dplyr::select(haul_id, set_year, set_month, set_day, set_time, up_year,
-                    up_month, up_day, up_time, d_date, r_date, fish_tickets) %>%
+                    up_month, up_day, up_time, d_date, r_date, fish_tickets, pcid,
+                    sector) %>%
       distinct()
     
+    # make times for set and up
+    set_time <- make_fullHrMin(v_obs$set_time) 
+    up_time <- make_fullHrMin(v_obs$up_time)
+    
+    set_datetime <- as.POSIXct(
+      paste0(v_obs$set_year,"-",v_obs$set_month,"-",v_obs$set_day," ", set_time),
+      format= "%Y-%m-%d %H:%M", tz = "Etc/GMT-8"
+    )
+    
+    up_datetime <- as.POSIXct(
+      paste0(v_obs$up_year,"-",v_obs$up_month,"-",v_obs$up_day," ", up_time),
+      format= "%Y-%m-%d %H:%M", tz = "Etc/GMT-8"
+    )
+    
+    # make depart and return real times
+    d_dates <- add_fullTime(v_obs$d_date)
+    r_dates <- add_fullTime(v_obs$r_date)
+    
+    trip_dat <- data.frame()
+    # keep track of trips
     for (j in 1:nrow(v_obs)){
-      ### Fishing event
-      SET <- v_obs[j,]
+      # Find vms between in and out times
+      f_id <- which(vms$date.time >= set_datetime[j] - 60*60 & 
+                      vms$date.time <= up_datetime[j] + 60*60)
       
-      ### Fishing gear in
-      yr <- SET$SET_YEAR
-      mt <- SET$SET_MONTH
-      dy <- SET$SET_DAY
-      hr <- SET$SET_TIME
-      mn <- (hr - floor(hr)) * 60
-      hr <- floor(hr)
-      TIME_in <- as.POSIXct(paste(yr,mt,dy,hr,mn,sep='-'),
-                            format="%Y-%m-%d-%H-%M",tz="Etc/GMT-8") - (1*60*60)
+      # add to fishing
+      vms$fishing[f_id] = as.character(v_obs$fish_tickets[j])
       
-      ### Fishing gear out
-      yr <- SET$UP_YEAR
-      mt <- SET$UP_MONTH
-      dy <- SET$UP_DAY
-      hr <- SET$UP_TIME
-      mn <- (hr - floor(hr)) * 60
-      hr <- floor(hr)
-      TIME_out <- as.POSIXct(paste(yr,mt,dy,hr,mn,sep='-'),
-                             format="%Y-%m-%d-%H-%M",tz="Etc/GMT-8") + (1*60*60)
+      # Was trip observerd
+      o_id <- which(vms$date.time >= d_dates[j] & 
+                      vms$date.time <= r_dates[j])
       
-      ### Find vms between in and out times
-      f_id <- which(data_vms$date.time >= TIME_in & data_vms$date.time <= TIME_out)
+      # add to observed list
+      vms$observed[o_id] = as.character(v_obs$fish_tickets[j])
+      vms$pcid[o_id] <- v_obs$pcid[j]
+      vms$sector[o_id] <- v_obs$sector[j]
       
-      ### add to fishing
-      fishing[f_id] = as.character(SET$FISH_TICKETS)
-      
-      
-      ### Observed start
-      d_date  <-  as.character(SET$D_DATE)
-      OBS_dep <-  as.POSIXct(d_date,format="%m/%d/%Y %I:%M:%S %p",tz="Etc/GMT-8")
-      
-      ### Observed end
-      r_date  <- as.character(SET$R_DATE)
-      OBS_ret <-  as.POSIXct(r_date,format="%m/%d/%Y %I:%M:%S %p",tz="Etc/GMT-8")
-      
-      ### Was trip observerd
-      o_id <- which(data_vms$date.time >= OBS_dep & data_vms$date.time <= OBS_ret)
-      
-      ### add to observed list
-      observed[o_id] = as.character(SET$FISH_TICKETS)
-      
-      ### Create histogram of haul times
-      #HAUL = rbind(HAUL,c(TIME_out-TIME_in,as.character(SET$COMMON_NAME)))
+      # add to table
+      trip_list <- data.frame(vessel.ID =gsub(".RDS","",names[i]),
+                     fish_ticket = v_obs$fish_tickets[j],
+                     sector = v_obs$sector[j], 
+                     pcid = v_obs$pcid[j],
+                     d_date = d_dates[j],
+                     r_date = r_dates[j],
+                     hrs_fishing = length(f_id),
+                     stringsAsFactors = FALSE)
+      trip_dat <- rbind(trip_dat, trip_list)
     }
     
-  ### fished and observed lists to vms dataframe
-  #data_vms = cbind(data_vms,fishing)
-  #data_vms = cbind(data_vms,observed)
-  save_vms <- data.frame(data_vms$longitude,data_vms$latitude,data_vms$date.time,data_vms$dist_coast,data_vms$only_trips,
-                    data_vms$metier.2010,fishing,observed,data_vms$trip_id1,data_vms$trip_id2,data_vms$trip_id3,data_vms$trip_id4,
-                    data_vms$trip_id5,data_vms$trip_id6, stringsAsFactors = FALSE)
-  
-  ### Save as new file
+    total_trip_dat <- rbind(total_trip_dat, trip_dat)
+    
+    any(!(is.na(vms$fishing)) & is.na(vms$observed)) # should be false
+    
+  # Save as new file
   len_name = nchar(infile)
   name = substr(infile,1,len_name-4)
-  if(user == "james") write.csv(save_vms,file=paste("./VMS/CSV/",name,".csv",sep = ""),row.names = FALSE)
-  if(user == "emma") write.csv(save_vms, file = paste0("/Users/efuller/1/CNH/processedData/spatial/vms/intermediate/05_make_obs_to_vms/",name,".csv"), row.names = FALSE)
+  write.csv(vms, file = paste0("processedData/spatial/vms/intermediate/05_make_obs_to_vms/",window,"hr/",name,".csv"), row.names = FALSE)
   
   }else{
 
-  save_vms <- dplyr::select(data_vms, longitude, latitude, date.time, 
-                            dist_coast, only_trips, metier.2010, fishing, 
-                            observed, contains("trip_id"))
-
   ### Save as new file
   file_name <- gsub(".RDS",".csv",names[i])
-  save_path <- paste0("/Users/efuller/Desktop/CNH/processedData/spatial/vms/intermediate/05_make_obs_to_vms/tw_",window,"hr/")
-  write.csv(save_vms, file = paste0(save_path,file_name), row.names = FALSE)
+  save_path <- paste0("processedData/spatial/vms/intermediate/05_make_obs_to_vms/tw_",window,"hr/")
+  write.csv(vms, file = paste0(save_path,file_name), row.names = FALSE)
   }
   
 }
