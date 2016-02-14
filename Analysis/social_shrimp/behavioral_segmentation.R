2573210d04e1ec4949efb2807d2200ff4d23e5ea
Emma Fuller <efuller@princeton.edu>
2016-02-13 18:04:08 -0800

    working on social shrimp behavioral segmentation, introduction freewrite

--- Analysis/social_shrimp/behavioral_segmentation.R
+++ Analysis/social_shrimp/behavioral_segmentation.R
@@ -1,5 +1,10 @@
 # load training data and train RF
 library(dplyr)
+library(ggplot2)
+library(broom)
+library(adehabitatLT)
+library(maps)
+library(sp)
 
 # look in trip_tot_dat to find vessel IDs that have observed shrimp trips ----
   # choose time window
@@ -22,27 +27,90 @@ fp <- paste0("processedData/spatial/vms/intermediate/05_make_obs_to_vms/trip_tot
     filter(!(is.na(observed)))
   
 # generate movement statistics ----
-  library(adehabitatLT)
+  
+  # need to make an ID that is only_trips + doc.num to get unique trips 
+  # other groupings may aggregate multiple trips. 
+  # shouldn't happen because observed, but does look like it's happening 
+  vms_files$date.time <- as.POSIXct(vms_files$date.time, tz = "Etc/GMT-8")
+  vms_files$trip_id1 <- as.character(vms_files$trip_id1)
+  vms_files <- vms_files[order(vms_files$doc.num, vms_files$date.time),]
+  vms_files$burst_id <- paste(vms_files$only_trips, vms_files$doc.num, sep="_")
+  
+  # test to make sure all trips correspond to one burst_id
+    # all_ids <- 1:length(unique(vms_files$trip_id1))
+    # n_burst = function(x){
+    #   return(length(unique(
+    #     subset(vms_files,
+    #            trip_id1==unique(vms_files$trip_id1)[x])$burst_id)))
+    # }
+    # 
+    # count_bursts = sapply(all_ids, n_burst)
+    # which(count_bursts>1)
+  prob_trips <- unique(vms_files$trip_id1)[which(count_bursts>1)]
+
+  with(subset(vms_files, trip_id1==prob_trips[1]),
+       plot(longitude, latitude, asp = 1, type='o',cex=.5,
+            col = as.factor(burst_id)))
+  map('state',add=T, col='darkolivegreen4', fill=T, bor = "grey")
+  
+  with(subset(vms_files, trip_id1==unique(vms_files$trip_id1)[]),
+       plot(longitude, latitude, asp = 1, type='o',cex=.5))
+  map('state',add=T, col='darkolivegreen4', fill=T, bor = "grey")
+  
   
   # make spatial
   coordinates(vms_files) <- ~longitude+latitude
   proj4string(vms_files) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
   
-  # probably should project
+  # probably should project, ltraj just does euclidean distance, 
+  # now distance in meters
+  # projection came from projectionwizard, is 
+  # Equidistant conic PROJ.4 - distance correct along meridians
+  #   Latitude of origin: 39º 26' N
+  #   Standard parallel 1: 33º 02' N
+  #   Standard parallel 2: 45º 50' N
+  #   Central meridian: 125º 04' W
   
-  vms_files$date.time <- as.POSIXct(vms_files$date.time, tz = "Etc/GMT-8")
-  vms_files$trip_id1 <- as.character(vms_files$trip_id1)
-  dat_lt <- as.ltraj(coordinates(vms_files), date = vms_files$date.time, 
-                     id = vms_files$trip_id1)
+  vms_proj <- spTransform(vms_files, 
+                          CRS("+proj=eqdc +lat_1=33.040181240749774 +
+                              lat_2=45.838330604411105 +lon_0=-125.068359375"))
+  # add coastline for ref
+  load("processedData/spatial/2_coastline.Rdata")
+  proj4string(WC) <- proj4string(vms_files)
+  WC <- spTransform(WC, proj4string(vms_proj))
   
+  vms_proj$date.time <- as.POSIXct(vms_proj$date.time, tz = "Etc/GMT-8")
+  vms_proj$trip_id1 <- as.character(vms_proj$trip_id1)
+  dat_lt <- as.ltraj(coordinates(vms_proj), date = vms_proj$date.time, 
+                     id = vms_proj$trip_id1)
   dat <- ld(dat_lt)
+  
   dat$id <- as.character(dat$id)
   # add observed and fishing data to dat
-  dat <- left_join(dat, dplyr::select(as.data.frame(vms_files), trip_id1, date.time, fishing, observed),by = c("id" = "trip_id1","date"="date.time"))
+  dat <- left_join(dat, dplyr::select(as.data.frame(vms_files), trip_id1, 
+                                      date.time, fishing, observed),
+                   by = c("id" = "trip_id1","date"="date.time"))
+  # add speed
   dat$speed <- dat$dist/dat$dt
   
 # make sure training data is clean ----
   # are there gaps in trips > 2 hours?
+  length(which(dat$dt>2*60*60))
+  
+  # subset to those trips
+  miss_trips <- unique(dat$id[which(dat$dt> 2*60*60)])
+  # about a fifth have this
+  length(miss_trips)/length(unique(dat$id))
+  
+  library(broom)
+  wc_g <- tidy(WC, region = "id")
+  ggplot(subset(dat, id %in% miss_trips), aes(x, y, group = id)) + 
+    geom_point(aes(color=id), size = .05) + theme(legend.position="None") + 
+    coord_equal() + 
+    geom_polygon(data = wc_g, aes(x = long, y = lat, group = group)) 
+
+  plot(dat$x, dat$y, asp = 1, col = ifelse(dat$dt>2*60*60, "red","grey"), 
+       cex = .15)
   
   # what are the NAs for dt and dist?
   
