99527f19acbe787eb62158d4a9c7a632d8d66e35
jameals <jameal.samhouri@noaa.gov>
2016-06-17 16:23:28 -0700

    Updated 03_overlapMetier for use with jameal's directories and changed headers doc.num --> docnum, date.time --> datetime

--- processedData/spatial/vms/intermediate/03_overlapMetier_VMS_2013_2015.R
+++ processedData/spatial/vms/intermediate/03_overlapMetier_VMS_2013_2015.R
@@ -1,14 +1,15 @@
 # match VMS to metiers
 
 # load data ----
-tickets <- readRDS("/Users/efuller/Desktop/CNH/processedData/catch/1_cleaningData/tickets.RDS")
+tickets <- readRDS("/Users/jameal.samhouri/Documents/CNH_to_github/cnh/processedData/catch/1_cleaningData/tickets.RDS") # /Users/efuller/Desktop/CNH/processedData/catch/1_cleaningData
+names(tickets)
 
 landings <- unique(tickets[,c("trip_id","drvid","tdate","metier.2010")])
 landings$tdate <- as.POSIXlt(landings$tdate, format = "%d-%b-%y", tz = "Etc/GMT-8")
 rm(tickets)
 
 # for each vessel track, check if it's in landings data. if not, next
-setwd("/Users/efuller/Desktop/CNH/processedData/spatial/vms/intermediate/02_cleaned/")
+setwd("/Users/jameal.samhouri/Documents/CNH_to_github/cnh/processedData/spatial/vms/intermediate/02_cleaned/")
 fish_tracks <- dir()
 
 # find out if VMS is in landing tickets ----
@@ -19,20 +20,20 @@ for(i in 1:length(fish_tracks)){
   # if there's no track 
   # (which happens because didn't properly filter for vessels that never leave 
   # land upstream in 02_cleaning_data.R)
-  doc.num <- unique(track$doc.num)
+  docnum <- unique(track$docnum)
   
-  if(length(doc.num)>1) break # shouldn't be the case. because split on doc.num earlier
+  if(length(docnum)>1) break # shouldn't be the case. because split on doc.num earlier
   
   # if the landing gear is there at all
-  if(doc.num %in% landings$drvid){ 
-    single_landing <- landings[(which(landings$drvid == doc.num)),]
+  if(docnum %in% landings$drvid){ 
+    single_landing <- landings[(which(landings$drvid == docnum)),]
     
     # check if there's any overlap between dates of VMS and landings
-    if(any(track$date.time > min(single_landing$tdate) & 
-           track$date.time < max(single_landing$tdate))){
+    if(any(track$datetime > min(single_landing$tdate) & 
+           track$datetime < max(single_landing$tdate))){
       
       saveRDS(track, paste0(
-        "/Users/efuller/Desktop/CNH/processedData/spatial/vms/intermediate/03_overlapMetier/v_",
-        unique(track$doc.num),".RDS")) }
+        "/Users/jameal.samhouri/Documents/CNH_to_github/cnh/processedData/spatial/vms/intermediate/03_overlapMetier/v_",
+        unique(track$docnum),".RDS")) }
   }
 }
