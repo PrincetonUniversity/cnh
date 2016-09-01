32bee489b3886c73df367d479cd939a62239885f
jameals <jameal.samhouri@noaa.gov>
2016-09-01 13:08:05 -0700

    open 2010-2015 pacfin data script

---
+++ processedData/catch/1_cleaningData/open pacfin2010_2015.R
@@ -0,0 +1,7 @@
+setwd("/Users/jamealsamhouri/Documents/cnh/")
+
+filename <- "J Samhouri - comp_ft _2010_2015"
+dat1 <- read.csv(paste0("rawData/Catch/2010-2015_pacfin_data/",filename,".csv"))
+
+colnames(dat1)
+head(dat1)
